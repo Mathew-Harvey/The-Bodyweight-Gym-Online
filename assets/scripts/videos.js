c4a8e728bb5021fa9d6ede52d1624fdb03d34c88
Andrew <mrbondmustdie@gmail.com>
2020-10-03 14:14:09 +0800

    Added basic layout of the search function, to be turned into code at a later date

--- assets/scripts/videos.js
+++ assets/scripts/videos.js
@@ -6,55 +6,63 @@ const $theVideos = {
 
         "link": "vimeo.com/463677938",
         "tags": ["Mobility"],
-        "coach": "Corey"
+        "coach": "Corey",
+        "screenshot": "./assets/img/previews/prev1.png"
     },
     // Video 2
     1: {
 
         "link": "vimeo.com/463679811",
         "tags": ["Mobility"],
-        "coach": "Corey"
+        "coach": "Corey",
+        "screenshot": "./assets/img/previews/prev2.png"
     },
     // Video 3
     2: {
 
         "link": "vimeo.com/463703807",
         "tags": ["Mobility"],
-        "coach": "Maisie"
+        "coach": "Maisie",
+        "screenshot": "./assets/img/previews/prev3.png"
     },
     // Video 4
     3: {
 
         "link": "vimeo.com/463687356",
         "tags": ["Mobility"],
-        "coach": "Maisie"
+        "coach": "Maisie",
+        "screenshot": "./assets/img/previews/prev4.png"
     },
     // Video 5
     4: {
 
         "link": "vimeo.com/463756659",
         "tags": ["Handstands"],
-        "coach": "Mikhail"
+        "coach": "Mikhail",
+        "screenshot": "./assets/img/previews/prev5.png"
     },
     // Video 6
     5: {
 
         "link": "vimeo.com/463722728",
         "tags": ["Strength"],
-        "coach": "Ray"
+        "coach": "Ray",
+        "screenshot": "./assets/img/previews/prev6.png"
     },
     // Video 7
     6: {
 
         "link": "vimeo.com/463719228",
         "tags": ["Strength"],
-        "coach": "Maisie"
+        "coach": "Maisie",
+        "screenshot": "./assets/img/previews/prev7.png"
     },
     // Video 8
     7: {
 
         "link": "vimeo.com/463716498",
         "tags": ["Strength"],
-        "coach": "Oscar"
+        "coach": "Oscar",
+        "screenshot": "./assets/img/previews/prev8.png"
     }
 }
