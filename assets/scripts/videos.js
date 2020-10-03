66537cb9ff596818b0ba2299b6b3006eee49e1a0
Andrew <mrbondmustdie@gmail.com>
2020-10-03 12:32:02 +0800

    Changed the tags to arrays, for future proofing

--- assets/scripts/videos.js
+++ assets/scripts/videos.js
@@ -5,56 +5,56 @@ const $theVideos = {
     0: {
 
         "link": "vimeo.com/463677938",
-        "tags": "Mobility",
+        "tags": ["Mobility"],
         "coach": "Corey"
     },
     // Video 2
     1: {
 
         "link": "vimeo.com/463679811",
-        "tags": "Mobility",
+        "tags": ["Mobility"],
         "coach": "Corey"
     },
     // Video 3
     2: {
 
         "link": "vimeo.com/463703807",
-        "tags": "Mobility",
+        "tags": ["Mobility"],
         "coach": "Maisie"
     },
     // Video 4
     3: {
 
         "link": "vimeo.com/463687356",
-        "tags": "Mobility",
+        "tags": ["Mobility"],
         "coach": "Maisie"
     },
     // Video 5
     4: {
 
         "link": "vimeo.com/463756659",
-        "tags": "Handstands",
+        "tags": ["Handstands"],
         "coach": "Mikhail"
     },
     // Video 6
     5: {
 
         "link": "vimeo.com/463722728",
-        "tags": "Strength",
+        "tags": ["Strength"],
         "coach": "Ray"
     },
     // Video 7
     6: {
 
         "link": "vimeo.com/463719228",
-        "tags": "Strength",
+        "tags": ["Strength"],
         "coach": "Maisie"
     },
     // Video 8
     7: {
 
         "link": "vimeo.com/463716498",
-        "tags": "Strength",
+        "tags": ["Strength"],
         "coach": "Oscar"
     }
 }
