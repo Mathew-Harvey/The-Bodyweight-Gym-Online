dc4307d58a6c81123c219dcf7e3ecf3ed7af2e32
Mathew <mathewharvey@gmail.com>
2020-10-04 21:05:45 +0800

    Finished timer, and metronome

--- assets/scripts/timerscript.js
+++ assets/scripts/timerscript.js
@@ -1,10 +1,18 @@
 
 var timerBtn = document.getElementById("showTimer")
 var timer = document.getElementsByClassName("timer")
-minutesCount = 0,secondCount = 0,centiSecondCount = 0
 var minutes = document.getElementById("minutes")
 var second = document.getElementById("second")
 var centiSecond = document.getElementById("centiSecond")
+
+var arnie = new Audio("./assets/audio/Arnie.wav");
+
+function arnieEnergy() { 
+  arnie.play();
+  
+}
+
+minutesCount = 0,secondCount = 0,centiSecondCount = 0
 
 $(document).ready(function() {
     $('#showTimer').click (function() {
@@ -27,6 +35,10 @@ function startSW() {
         secondCount += 1
         if(secondCount >59){
             secondCount = 1
+        }
+        if (secondCount == 45) {
+            arnieEnergy();
+            arnie.volume = 0.4;
         }
         second.innerHTML = secondCount
     },1000)
@@ -92,3 +104,48 @@ $(document).ready(function() {
 });
 
 
+// Make timer dragable
+
+
+dragElement(document.getElementById("timerEl"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
