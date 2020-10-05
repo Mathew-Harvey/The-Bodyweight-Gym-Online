37715f8274c24f753089da8578074efa5089a473
Mark Wilson <markwilson107@hotmail.com>
2020-10-05 21:31:20 +0800

    User profile login finished and weather loads from profile local storage

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,5 +1,7 @@
-function loadWeather(cityName) {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=256b25d2b8a8f2b130f652e84d69f8ee";
+if (localStorage.getItem("userProfile") != null)
+{
+    profileObj = JSON.parse(localStorage.getItem("userProfile"))
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${profileObj.location}&units=metric&appid=256b25d2b8a8f2b130f652e84d69f8ee`;
     $.ajax({
         url: url,
         success: function (data) {
