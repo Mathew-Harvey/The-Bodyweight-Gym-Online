a017be3b566912e06a6efc4604699b9874a37a80
Andrew <mrbondmustdie@gmail.com>
2020-10-07 19:07:27 +0800

    Added local storage for likes and complete

--- assets/scripts/shared.js
+++ assets/scripts/shared.js
@@ -34,14 +34,68 @@ function checkCoach(which) {
     return "";
 }
 
-function heartVideo(which){
-    
+// Function to "heart" a video
+function heartVideo(which) {
+
     var $arrayPos = $.inArray(which, $likedVideos);
 
-    if ($arrayPos == -1){
+    if ($arrayPos == -1) {
         $likedVideos.push(which);
     }
     else {
-        $likedVideos.splice($arrayPos,1);
+        $likedVideos.splice($arrayPos, 1);
+    }
+
+    // Update the history
+    updateStorage();
+}
+
+// Function to mark a video as complete
+function finishVideo(which) {
+
+    var $arrayPos = $.inArray(which, $watchedVideos);
+
+    if ($arrayPos == -1) {
+        $watchedVideos.push(which);
+    }
+
+    // Update the history
+    updateStorage();
+}
+
+// Function to retrieve the stored history
+function retrieveStoredHistory() {
+
+    // Call to check for local storage
+    var $checkExistingHistory = localStorage.getItem("bodyweightgym-history");
+    var $checkExistingLikes = localStorage.getItem("bodyweightgym-likes");
+
+    // If the history isn't empty
+    if ($checkExistingHistory != null) {
+        $watchedVideos = JSON.parse($checkExistingHistory);
+    }
+    if ($checkExistingLikes != null) {
+        $likedVideos = JSON.parse($checkExistingLikes);
     }
 }
+
+// Function to update the storage with latest video history
+function updateStorage() {
+
+    // Stringify the $watchedVideos & $likedVideos
+    var $addHistory = JSON.stringify($watchedVideos);
+    var $addLikes = JSON.stringify($likedVideos);
+
+    // Add to the local storage
+    localStorage.setItem("bodyweightgym-history", $addHistory);
+    localStorage.setItem("bodyweightgym-likes", $addLikes);
+
+    // Console log the updated search history
+    console.log("[SYSTEM] Added to local storage: " + $addHistory);
+    console.log("[SYSTEM] Added to local storage: " + $addLikes);
+}
+
+$(document).ready(function () {
+
+    retrieveStoredHistory();
+})
