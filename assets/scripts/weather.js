0ce6ede6accea6b3898217b884c5f229f41433d3
Mat <67828114+Mathew-Harvey@users.noreply.github.com>
2020-10-08 16:00:18 +0800

    Update weather.js

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,12 +1,1 @@
-function loadWeather(cityName) {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=256b25d2b8a8f2b130f652e84d69f8ee";
-    $.ajax({
-        url: url,
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (e) {
-            console.log(e.responseText);
-        }
-    });
-}
+
