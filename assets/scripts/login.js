e2500d292ebbfb8f4b0a440ca2a320a5196ecfcf
Mark Wilson <markwilson107@hotmail.com>
2020-10-12 17:51:34 +0800

    Added account dropdown menu.

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,9 +1,6 @@
 // Checks if user profile exists in local storage
 if (localStorage.getItem("userProfile") === null) {
     // update navbar buttons
-    if (on_index === true) {
-        $(".modal").addClass("is-active")
-    }
     $(".user-profile").css({ display: "none" });
     $(".favorites-btn").css({ display: "none" });
     $(".signup").css({ display: "block" });
@@ -17,21 +14,21 @@ if (localStorage.getItem("userProfile") === null) {
 }
 
 // Check if user submits login
-$("#login-submit").on("click", function() {
+$("#login-submit").on("click", function () {
     if ($("#login-name").val() != "" && $("#login-location").val() != "") {
         var url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#login-location").val()}&units=metric&appid=256b25d2b8a8f2b130f652e84d69f8ee`;
         $.ajax({
             url: url,
-            success: function(data) {
+            success: function (data) {
                 // create object
                 var profileObj = {
-                        name: $("#login-name").val(),
-                        location: data.name
-                    }
-                    // save object to local storage
+                    name: $("#login-name").val(),
+                    location: data.name
+                }
+                // save object to local storage
                 localStorage.setItem("userProfile", JSON.stringify(profileObj));
                 // update navbar buttons
-                $(".modal").removeClass("is-active");
+                $(".modal-login").removeClass("is-active");
                 $(".user-profile").css({ display: "block" });
                 $(".signup").css({ display: "none" });
                 $(".userProfile-name").html(`Welcome, <strong>${profileObj.name}</strong>`);
@@ -45,7 +42,7 @@ $("#login-submit").on("click", function() {
                     $("#weather-txt").html("Train <strong>inside</strong> today");
                 }
             },
-            error: function() {
+            error: function () {
                 $("#login-error").text("City not found. Please try again.");
             }
         });
@@ -55,20 +52,47 @@ $("#login-submit").on("click", function() {
 })
 
 // make sure all scripts are loaded before executing
-$(document).ready(function() {
+$(document).ready(function () {
     // check if create profile is clicked
-    $("#create-profile").on("click", function(event) {
+    $("#create-profile").on("click", function (event) {
         event.preventDefault();
-        $(".modal").addClass("is-active");
+        $(".modal-login").addClass("is-active");
     })
 
     // check if create profile is clicked
-    $(".signup").on("click", function() {
-        $(".modal").addClass("is-active")
+    $(".signup").on("click", function () {
+        $(".modal-login").addClass("is-active");
     })
 
     // check close login modal
-    $(".modal-close").on("click", function(event) {
-        $(".modal").removeClass("is-active");
+    $(".modal-close").on("click", function (event) {
+        $(".modal-login").removeClass("is-active");
     })
+
+    // check if account button is clicked
+    $('.account-dropdown').on('click',function(event){
+        event.preventDefault();
+        if($(this).attr('data-click-state') == 1) {
+            $(this).attr('data-click-state', 0);
+            $(".account-dropdown").addClass("is-active");
+          }
+        else {
+          $(this).attr('data-click-state', 1);
+          $(".account-dropdown").removeClass("is-active");
+        }
+    });
+
+    // check if new account button is clicked
+    $(".new-account-btn").on("click", function() {
+        $(".modal-login").addClass("is-active");
+    })
+
+    // check if delete account button is clicked
+    $(".delete-account-btn").on("click", function() {
+        localStorage.removeItem("userProfile");
+        localStorage.removeItem("bodyweightgym-history");
+        localStorage.removeItem("bodyweightgym-likes");
+        location.reload();
+    })
+
 })
