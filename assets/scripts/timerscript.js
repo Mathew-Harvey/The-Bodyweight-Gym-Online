b3f8c03539976e77aa0658c88b935eb438a50946
Andrew <mrbondmustdie@gmail.com>
2020-10-12 18:07:35 +0800

    Checking touch drag and drop

--- assets/scripts/timerscript.js
+++ assets/scripts/timerscript.js
@@ -130,9 +130,11 @@ function dragElement(elmnt) {
     if (document.getElementById(elmnt.id + "header")) {
         /* if present, the header is where you move the DIV from:*/
         document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        document.getElementById(elmnt.id + "header").ontouchstart = dragMouseDown;
     } else {
         /* otherwise, move the DIV from anywhere inside the DIV:*/
         elmnt.onmousedown = dragMouseDown;
+        elmnt.ontouchstart = dragMouseDown;
     }
 
     function dragMouseDown(e) {
@@ -142,8 +144,10 @@ function dragElement(elmnt) {
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
+        document.ontouchend = closeDragElement;
         // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
+        document.ontouchmove  = elementDrag;
     }
 
     function elementDrag(e) {
@@ -162,6 +166,8 @@ function dragElement(elmnt) {
     function closeDragElement() {
         /* stop moving when mouse button is released:*/
         document.onmouseup = null;
-        document.onmousemove = null;
+        document.ontouchend = null;
+        document.onmouseup = null;
+        document.ontouchmove = null;
     }
 }
